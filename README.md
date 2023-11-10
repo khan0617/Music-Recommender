b06fdf31705cdb52308af4f157565944f514ba4c
Hamza Khan <hamza55014@gmail.com>
2023-11-10 12:40:26 -0600

    Initial commit, intermediate solution

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Music-Recommender
+### A similar song finder using KNearestNeighbors, the Spotify API, and a CUDA acceleration.
---
+++ my_k_neighbors_classifier.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+class MyKNeighborsClassifier:
+    def __init__(self, k: int, dist_metric: str) -> None:
+        self.k = k
+        self.dist_metric = dist_metric 
+
+    def _euclidean(record, data):
+        pass
+
+    def fit(self, X, y):
+        # TODO
+        pass
+
+    def predict(self, X):
+        # TODO
+        pass
