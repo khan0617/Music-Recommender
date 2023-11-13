5085ebe14f61b4064a0e057f9ccf58e8e10969cc
Hamza Khan <hamza55014@gmail.com>
2023-11-12 20:31:30 -0600

    Add Initial designs for html album components

--- static/js/music_recommender.js
+++ static/js/music_recommender.js
@@ -1,3 +1,7 @@
+// Adjust the autocomplete suggestions' width on page load and window resize
+window.addEventListener('load', adjustSuggestionsWidth);
+window.addEventListener('resize', adjustSuggestionsWidth);
+
 // Initiate a searchSong request when the user hits Enter
 document.getElementById('searchInput').addEventListener('keydown', (event) => {
     if (event.code === 'Enter') {
@@ -12,13 +16,30 @@ document.getElementById('searchInput').addEventListener('keydown', (event) => {
     }
 });
 
+// let autocomplete suggestions area scale dynamically with the search bar and page
+function adjustSuggestionsWidth() {
+    let inputWidth = document.getElementById('searchInput').offsetWidth;
+    let suggestionsContainer = document.getElementById('suggestionsContainer');
+    suggestionsContainer.style.width = inputWidth + 48 + 'px';
+}
+
 // initiate a searchSong request for the specified song.
-function searchSong(songQuery) {
+/**
+ * @param {string} songQuery
+ * @param {boolean} fromAutocomplete
+ */
+function searchSong(songQuery, fromAutocomplete = false) {
     if (songQuery) {
-        fetch('/searchSong?query=' + encodeURIComponent(songQuery))
+        // autocomplete requests will have a "by {artist}" append to the song name.
+        // we can use this information in the search.
+        let url = '/searchSong?query=' + encodeURIComponent(songQuery);
+        if (fromAutocomplete) {
+            url += '&fromAutocomplete=true';
+        }
+
+        fetch(url)
             .then(response => response.json())  // Parse the JSON response
             .then(data => {
-                // Log the search results to the console
                 console.log('Search results:', data);
             })
             .catch(error => console.error('Error searching song:', error));
@@ -31,7 +52,7 @@ function searchSong(songQuery) {
 function selectSuggestion(suggestion) {
     document.getElementById('searchInput').value = suggestion;
     clearSuggestions();
-    searchSong(suggestion);  // Trigger the search when a suggestion is clicked
+    searchSong(suggestion, fromAutocomplete=true);  // Trigger the search when a suggestion is clicked
 }
 
 // Fetches autocomplete suggestions for the search bar
